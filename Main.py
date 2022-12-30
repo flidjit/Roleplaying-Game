163a8849c55996a7aaf829f519bee5807370c3c7
Bobby <flidjit@gmail.com>
2022-12-30 01:17:49 -0500

    Double sure.
    Now we have two working pushes in a row!

--- Main.py
+++ Main.py
@@ -1,53 +1,9 @@
 import tkinter as tk
+import proto as pt
 from direct.showbase.ShowBase import ShowBase
 from direct.showbase.ShowBaseGlobal import globalClock
 from direct.task.TaskManagerGlobal import taskMgr
 from panda3d.core import WindowProperties, OrthographicLens, NodePath
-
-movement_data = {
-    "Shift Index": ["NW", "NE", "SW", "SE"],
-    "NE": [-5, -5, -45],
-    "NE Move": [0.05, -0.05, 0.05, -0.05],
-    "NW": [5, -5, 45],
-    "NW Move": [0.05, -0.05, -0.05, 0.05],
-    "SW": [-5, 5, -135],
-    "SW Move": [-0.05, 0.05, 0.05, -0.05],
-    "SE": [5, 5, 135],
-    "SE Move": [-0.05, 0.05, -0.05, 0.05]}
-
-
-class Thing3D:
-    def __init__(self, x=0, y=0, z=0):
-        self.name = 'Thing'
-        self.model_name = 'catalog/terrain/basic/Floor1.gltf'
-        self.this_instance = None
-        self.facing = 0
-        self.draw_at = [x, y, z]
-        self.tags = []
-
-
-class GridSquare(Thing3D):
-    def __init__(self, x=0, y=0, z=0):
-        Thing3D.__init__(self, x=x, y=y, z=0)
-        self.terrain_type = "Impassable"
-        self.occupied_by = None
-
-
-class MapChunk:
-    def __init__(self):
-        self.name = ''
-        self.terrain_model_list = []
-        self.character_list = []
-        self.grid = [[GridSquare(x, y) for x in range(20)] for y in range(20)]
-
-
-class Character(Thing3D):
-    def __init__(self):
-        Thing3D.__init__(self)
-        self.player = {}
-        self.stats = {}
-        self.scores = {}
-        self.skills = {}
 
 
 class GameWindow(ShowBase):
@@ -66,7 +22,6 @@ class GameWindow(ShowBase):
         props.setParentWindow(self.image_frame.winfo_id())
         props.setOrigin(0, 0)
         props.setSize(800, 400)
-
         self.win = base.makeDefaultPipe()
         base.openDefaultWindow(props=props)
 
@@ -80,44 +35,24 @@ class GameWindow(ShowBase):
         self.set_camera_position()
         self.set_zoom()
 
-        self.current_map = MapChunk()
+        self.current_map = pt.MapChunk()
         self.terrain_models = {}
         self.mod_lib = NodePath('model-library')
         self.instantiate_map()
 
-        self.keyMap = {
-            "up": False,
-            "down": False,
-            "left": False,
-            "right": False,
-            "space": False,
-            "control": False,
-            "e": False,
-            "q": False }
-        self.accept("arrow_up", self.update_key_map, ["up", True])
-        self.accept("arrow_up-up", self.update_key_map, ["up", False])
-        self.accept("arrow_down", self.update_key_map, ["down", True])
-        self.accept("arrow_down-up", self.update_key_map, ["down", False])
-        self.accept("arrow_left", self.update_key_map, ["left", True])
-        self.accept("arrow_left-up", self.update_key_map, ["left", False])
-        self.accept("arrow_right", self.update_key_map, ["right", True])
-        self.accept("arrow_right-up", self.update_key_map, ["right", False])
-        self.accept("space", self.update_key_map, ["space", True])
-        self.accept("space-up", self.update_key_map, ["space", False])
-        self.accept("control", self.update_key_map, ["control", True])
-        self.accept("control-up", self.update_key_map, ["control", False])
-        self.accept("e", self.update_key_map, ["e", True])
-        self.accept("e-up", self.update_key_map, ["e", False])
-        self.accept("q", self.update_key_map, ["q", True])
-        self.accept("q-up", self.update_key_map, ["q", False])
+        self.keyMap = {}
+        self.configure_keys()
         self.updateTask = taskMgr.add(self.update, "update")
 
     def update_key_map(self, controlName, controlState):
         self.keyMap[controlName] = controlState
 
     def configure_keys(self):
-        for i in keylist:
-            self.accept(keylist[i], self.update_key_map, [keylist[i]])
+        k = pt.keyboard_list
+        for i in range(len(k)):
+            self.keyMap[k[i][1]] = False
+            self.accept(k[i][0], self.update_key_map, [k[i][1], True])
+            self.accept(k[i][0]+'-up', self.update_key_map, [k[i][1], False])
 
     def instantiate_map(self):
         self.character = self.loader.loadModel("Images/models1.gltf")
@@ -136,8 +71,8 @@ class GameWindow(ShowBase):
         t = [self.camera_target[0]+self.target_offset[0],
              self.camera_target[1]+self.target_offset[1],
              self.camera_target[2]+self.target_offset[2]]
-        d = movement_data[movement_data["Shift Index"][self.camera_direction_index]]
-        print(movement_data["Shift Index"][self.camera_direction_index])
+        d = pt.movement_data[pt.movement_data["Shift Index"][self.camera_direction_index]]
+        print(pt.movement_data["Shift Index"][self.camera_direction_index])
         self.cam.setPosHpr(t[0]+d[0], t[1]+d[1], t[2]+7.5, d[2], -45, 0)
 
     def shift_camera_rotation_index(self, direction='Right'):
@@ -158,7 +93,7 @@ class GameWindow(ShowBase):
 
     def update(self, task):
         self.root.update()
-        f = movement_data[movement_data["Shift Index"][self.camera_direction_index] + " Move"]
+        f = pt.movement_data[pt.movement_data["Shift Index"][self.camera_direction_index] + " Move"]
         if self.camera_direction_index == 0 or self.camera_direction_index == 2:
             if self.keyMap["up"]:
                 self.target_offset[1] += f[0]
---
+++ proto.py
@@ -0,0 +1,51 @@
+keyboard_list = [
+    ["arrow_up", "up"], ["arrow_down", "down"],
+    ["arrow_left", "left"], ["arrow_right", "right"],
+    ["space", "space"], ["control", "control"],
+    ["e", "e"], ["q", "q"]]
+
+
+movement_data = {
+    "Shift Index": ["NW", "NE", "SW", "SE"],
+    "NE": [-5, -5, -45],
+    "NE Move": [0.05, -0.05, 0.05, -0.05],
+    "NW": [5, -5, 45],
+    "NW Move": [0.05, -0.05, -0.05, 0.05],
+    "SW": [-5, 5, -135],
+    "SW Move": [-0.05, 0.05, 0.05, -0.05],
+    "SE": [5, 5, 135],
+    "SE Move": [-0.05, 0.05, -0.05, 0.05]}
+
+
+class Thing3D:
+    def __init__(self, x=0, y=0, z=0):
+        self.name = 'Thing'
+        self.model_name = 'catalog/terrain/basic/Floor1.gltf'
+        self.this_instance = None
+        self.facing = 0
+        self.draw_at = [x, y, z]
+        self.tags = []
+
+
+class GridSquare(Thing3D):
+    def __init__(self, x=0, y=0, z=0):
+        Thing3D.__init__(self, x=x, y=y, z=0)
+        self.terrain_type = "Impassable"
+        self.occupied_by = None
+
+
+class MapChunk:
+    def __init__(self):
+        self.name = ''
+        self.terrain_model_list = []
+        self.character_list = []
+        self.grid = [[GridSquare(x, y) for x in range(20)] for y in range(20)]
+
+
+class Character(Thing3D):
+    def __init__(self):
+        Thing3D.__init__(self)
+        self.player = {}
+        self.stats = {}
+        self.scores = {}
+        self.skills = {}
