33b42626f90f70137371bded6379d5c5e99ce546
Bobby <flidjit@gmail.com>
2022-12-29 19:25:22 -0500

    lost
    I don't know what just happened

--- Main.py
+++ Main.py
@@ -18,30 +18,29 @@ movement_data = {
     "SE Move": [-0.05, 0.05, -0.05, 0.05]}
 
 
-class MapChunk:
-    def __init__(self):
-        self.name = ''
-        self.terrain_model_list = []
-        self.terrain_models = {}
-        self.character_list = []
-        self.characters = {}
-        self.grid = [[]]
-
-
 class Thing3D:
-    def __init__(self):
+    def __init__(self, x=0, y=0, z=0):
         self.name = 'Thing'
-        self.model_name = ''
+        self.model_name = 'catalog/terrain/basic/Floor1.gltf'
+        self.this_instance = None
         self.facing = 0
-        self.draw_at = [0, 0, 0]
+        self.draw_at = [x, y, z]
         self.tags = []
 
 
 class GridSquare(Thing3D):
-    def __init__(self):
-        Thing3D.__init__(self)
-        self.terrain_type = "Impassible"
+    def __init__(self, x=0, y=0, z=0):
+        Thing3D.__init__(self, x=x, y=y, z=0)
+        self.terrain_type = "Impassable"
         self.occupied_by = None
+
+
+class MapChunk:
+    def __init__(self):
+        self.name = ''
+        self.terrain_model_list = []
+        self.character_list = []
+        self.grid = [[GridSquare(x, y) for x in range(20)] for y in range(20)]
 
 
 class Character(Thing3D):
@@ -71,6 +70,10 @@ class MyApp(ShowBase):
         self.camera_direction_index = 1
         self.set_camera_position()
         self.cooldown = {"camera rotation": 0}
+
+        self.current_map = MapChunk()
+        self.terrain_models = {}
+        self.instantiate_map()
 
         self.scene = self.loader.loadModel("Images/models1.gltf")
         self.scene.reparentTo(self.render)
@@ -106,6 +109,16 @@ class MyApp(ShowBase):
 
     def updateKeyMap(self, controlName, controlState):
         self.keyMap[controlName] = controlState
+
+    def instantiate_map(self):
+        for i in range(20):
+            for j in range(20):
+                nam = self.current_map.grid[i][j].model_name
+                if nam not in self.terrain_models:
+                    self.terrain_models[nam] = self.loader.loadModel(nam)
+                self.current_map.grid[i][j].this_instance = self.render.attachNewNode(nam)
+                self.current_map.grid[i][j].this_instance.setPos(i, j, 0)
+                self.terrain_models[nam].instanceTo(self.current_map.grid[i][j].this_instance)
 
     def set_camera_position(self):
         t = [self.camera_target[0]+self.target_offset[0],
